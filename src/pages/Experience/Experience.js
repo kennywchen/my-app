097aee2bc8fdc85c034440f042aabe3056469653
kennywchen <kennywchen123@gmail.com>
2022-10-02 23:58:42 -0700

    home page typing animation

--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -2,32 +2,36 @@ import React from "react";
 import "./Experience.css"
 import 'animate.css'
 import ExperienceItem from "../../components/experienceItem/experienceItem";
+import Fade from 'react-reveal/Fade';
 
 export default function Experience(){
     return(
         <div>
-            <div id="Experience" className="text">
-                <div id="title" className="animate__animated animate__fadeInLeft">experiences</div>
-                <div id="resume" className="animate__animated animate__fadeInLeft">
-                        <ExperienceItem
-                            date="fa'22"
-                            role="mentored developer @ berkeley codebase"
-                        />
-                        <div id="item"> 
-                            <div id="date">sp'21 - su'22</div>
-                            <div id="role">research assistant @ stanford univeristy, <br></br>department of structural biology</div>
-                            <hr id="line"></hr>
-                        </div>
-                        <ExperienceItem
-                            date="su'21"
-                            role="research student @ uci cosmos summer program"
-                        />
-                        <ExperienceItem
-                            date="sp'20"
-                            role="editor in chief @ 'The Best Algebra 2/Pre-Calculus Book Ever'"
-                        />
+            <Fade left>
+                <div id="Experience" className="text">
+                    <div id="title" className="animate__animated animate__fadeInLeft">experiences</div>
+                    <div id="resume" className="animate__animated animate__fadeInLeft">
+                            <ExperienceItem
+                                date="fa'22"
+                                role="mentored developer @ berkeley codebase"
+                            />
+                            <div id="item"> 
+                                <div id="date">sp'21 - su'22</div>
+                                <div id="role">research assistant @ stanford univeristy, <br></br>department of structural biology</div>
+                                <hr id="line"></hr>
+                            </div>
+                            <ExperienceItem
+                                date="su'21"
+                                role="research student @ uci cosmos summer program"
+                            />
+                            <ExperienceItem
+                                date="sp'20"
+                                role="editor in chief @ 'The Best Algebra 2/Pre-Calculus Book Ever'"
+                            />
+                    </div>
                 </div>
-            </div>
+            </Fade>
+            
             <div className="dot3"></div>
             <div className="dot4"></div>
         </div>
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,15 +1,15 @@
 import React from "react";
-import "./Home.css"
-import 'animate.css'
-
+import "./Home.css";
+import 'animate.css';
 
 export default function Home(){
     return(
         <div className="home">
             <div id="home" className="animate__animated animate__fadeInLeft home-section">
-                <div id="intro">hello!</div>
-                <div id="name">i'm kenny</div>
-                <div id="bio">i'm a freshman at uc berkeley studying <br></br>computer science</div>
+                <div className="intro anim-typewriter">hello!</div>
+                <div id="name" className="anim-typewriter1">i'm kenny</div>
+                <div id="bio" className="anim-typewriter2">i'm a freshman at uc berkeley studying</div>
+                <div id="bio1" className="anim-typewriter3">computer science</div>
             </div>
             <div className="dot1"></div>
             <div className="dot2"></div>
